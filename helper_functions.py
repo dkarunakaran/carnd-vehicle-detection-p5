9a27bf4e462b173a86c116f69938cb6965c1ef0c
Dhanoop Karunakaran <dhanoop.karunakaran@gmail.com>
2017-12-10 10:10:54 +1100

    changes

--- helper_functions.py
+++ helper_functions.py
@@ -2,9 +2,12 @@ import matplotlib.image as mpimg
 import numpy as np
 import cv2
 from skimage.feature import hog
+
 # Define a function to return HOG features and visualization
 def get_hog_features(img, orient, pix_per_cell, cell_per_block, 
                         vis=False, feature_vec=True):
+    
+    
     # Call with two outputs if vis==True
     if vis == True:
         features, hog_image = hog(img, orientations=orient, 
@@ -24,6 +27,7 @@ def get_hog_features(img, orient, pix_per_cell, cell_per_block,
 
 # Define a function to compute binned color features  
 def bin_spatial(img, size=(32, 32)):
+    
     # Use cv2.resize().ravel() to create the feature vector
     features = cv2.resize(img, size).ravel() 
     # Return the feature vector
@@ -32,6 +36,7 @@ def bin_spatial(img, size=(32, 32)):
 # Define a function to compute color histogram features 
 # NEED TO CHANGE bins_range if reading .png files with mpimg!
 def color_hist(img, nbins=32, bins_range=(0, 256)):
+    
     # Compute the histogram of the color channels separately
     channel1_hist = np.histogram(img[:,:,0], bins=nbins, range=bins_range)
     channel2_hist = np.histogram(img[:,:,1], bins=nbins, range=bins_range)
@@ -96,3 +101,27 @@ def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
         cv2.rectangle(imcopy, bbox[0], bbox[1], color, thick)
     # Return the image copy with boxes drawn
     return imcopy
+
+# Function to Convert image to desired color space
+def convert_image(image, cspace):
+    '''''
+    Param: input image in RGB format
+    Param: Desired image colorspace
+    Returns: image in desired colorspace
+    '''''
+    # apply color conversion if other than 'RGB'
+    if cspace != 'RGB':
+        if cspace == 'HSV':
+            feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+        elif cspace == 'LUV':
+            feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2LUV)
+        elif cspace == 'HLS':
+            feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+        elif cspace == 'YUV':
+            feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
+        elif cspace == 'YCrCb':
+            feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YCrCb)
+    else: 
+        feature_image = np.copy(image) 
+        
+    return feature_image
